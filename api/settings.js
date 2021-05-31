d537214ad3ba56e9ec6584ee6ff6b592f987e595
제스퍼 <41337427+Ukong0324@users.noreply.github.com>
2021-05-31 22:14:17 +0900

    Update settings.js

--- api/settings.js
+++ api/settings.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    port: 443,
+    port: 80,
     ip: "localhost",
     mongo: {
         url: "mongodb://server:dbqls2011!@141.164.46.189:27017/admin?retryWrites=true&w=majority"
