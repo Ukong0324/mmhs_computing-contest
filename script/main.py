aec07d7f3a00a16cbf42fa5c2c8d3e19b159045c
제스퍼 <41337427+Ukong0324@users.noreply.github.com>
2021-06-02 00:19:34 +0900

    Create main.py
    Analyze data using Python 💞

---
+++ script/main.py
@@ -0,0 +1,10 @@
+# 파이썬을 이용하여 데이터 분석
+# https://corona-api.xyz < github repo redirect
+
+import requests, json
+
+res = requests.get('https://corona-api.xyz/api/korea')
+data = json.loads(res.text)
+
+print(f"{data}")
+
