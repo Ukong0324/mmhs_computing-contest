e1e12f99526e17b15923f5008a6678dc8d04e6cd
제스퍼 <41337427+Ukong0324@users.noreply.github.com>
2021-06-04 14:01:23 +0900

    Update main.py
    [Commit (aec07d7)](https://github.com/Ukong0324/mmhs_computing-contest/commit/aec07d7f3a00a16cbf42fa5c2c8d3e19b159045c#commitcomment-51607174)

--- script/main.py
+++ script/main.py
@@ -1,7 +1,8 @@
 # 파이썬을 이용하여 데이터 분석
 # https://corona-api.xyz < github repo redirect
 
-import requests, json
+import requests
+import json
 
 res = requests.get('https://corona-api.xyz/api/korea')
 data = json.loads(res.text)
